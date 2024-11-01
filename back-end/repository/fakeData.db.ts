ea335107fae9513e9d33a41680428072f38a5fc2
Roel Crabbé <crabberoel@gmail.com>
2024-11-01 13:13:47 +0100

    more fakedata

--- back-end/repository/fakeData.db.ts
+++ back-end/repository/fakeData.db.ts
@@ -6,26 +6,38 @@ import { WorkSchedule } from "../model/workSchedule";
 import { Color } from "../types";
 
 // Define projects
-const projectA = new Project({
-    id: 1,
-    name: 'Project Alpha',
-    color: Color.Red,
-    users: []
-});
-
-const projectB = new Project({
-    id: 2,
-    name: 'Project Beta',
-    color: Color.Green,
-    users: []
-});
-
-const projectC = new Project({
-    id: 3,
-    name: 'Project Gamma',
-    color: Color.Blue,
-    users: []
-});
+const projects: Project[] = [
+    new Project({
+        id: 1,
+        name: 'Project Alpha',
+        color: Color.Red,
+        users: []
+    }),
+    new Project({
+        id: 2,
+        name: 'Project Beta',
+        color: Color.Green,
+        users: []
+    }),
+    new Project({
+        id: 3,
+        name: 'Project Gamma',
+        color: Color.Blue,
+        users: []
+    }),
+    new Project({
+        id: 4,
+        name: 'Project Delta',
+        color: Color.Yellow,
+        users: []
+    }),
+    new Project({
+        id: 5,
+        name: 'Project Epsilon',
+        color: Color.Orange,
+        users: []
+    })
+];
 
 // Temporary work schedule for users
 const tempWorkSchedule = new WorkSchedule({
@@ -41,110 +53,208 @@ const tempWorkSchedule = new WorkSchedule({
 });
 
 // Define users
-const user1 = new User({
-    id: 1,
-    username: 'Johnny Sinister',
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'jdoe@example.com',
-    password: 'password123',
-    role: 'admin',
-    projects: [projectA, projectB],
-    workDays: [],
-    workSchedule: tempWorkSchedule // Temporary
-});
-
-const user2 = new User({
-    id: 2,
-    username: 'JaneDynamic',
-    firstName: 'Jane',
-    lastName: 'Smith',
-    email: 'jsmith@example.com',
-    password: 'password456',
-    role: 'guest',
-    projects: [projectB, projectC],
-    workDays: [],
-    workSchedule: tempWorkSchedule // Temporary
-});
-
-const user3 = new User({
-    id: 3,
-    username: 'SamCreative',
-    firstName: 'Sam',
-    lastName: 'Lee',
-    email: 'slee@example.com',
-    password: 'password789',
-    role: 'guest',
-    projects: [projectA, projectC],
-    workDays: [],
-    workSchedule: tempWorkSchedule // Temporary
-});
+const users: User[] = [
+    new User({
+        id: 1,
+        username: 'JohnnySinister',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'jdoe@example.com',
+        password: 'password123',
+        role: 'admin',
+        projects: [projects[0], projects[1]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    }),
+    new User({
+        id: 2,
+        username: 'JaneDynamic',
+        firstName: 'Jane',
+        lastName: 'Smith',
+        email: 'jsmith@example.com',
+        password: 'password456',
+        role: 'guest',
+        projects: [projects[0], projects[1], projects[2]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    }),
+    new User({
+        id: 3,
+        username: 'SamCreative',
+        firstName: 'Sam',
+        lastName: 'Lee',
+        email: 'slee@example.com',
+        password: 'password789',
+        role: 'guest',
+        projects: [projects[0], projects[2]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    }),
+    new User({
+        id: 4,
+        username: 'AlexBuilder',
+        firstName: 'Alex',
+        lastName: 'Johnson',
+        email: 'ajohnson@example.com',
+        password: 'password321',
+        role: 'admin',
+        projects: [projects[0], projects[3], projects[4]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    }),
+    new User({
+        id: 5,
+        username: 'ChrisInnovator',
+        firstName: 'Chris',
+        lastName: 'Brown',
+        email: 'cbrown@example.com',
+        password: 'password654',
+        role: 'student',
+        projects: [projects[0], projects[1], projects[4]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    }),
+    new User({
+        id: 6,
+        username: 'TaylorVisionary',
+        firstName: 'Taylor',
+        lastName: 'Green',
+        email: 'tgreen@example.com',
+        password: 'password987',
+        role: 'guest',
+        projects: [projects[0], projects[2], projects[3]],
+        workDays: [],
+        workSchedule: tempWorkSchedule
+    })
+];
 
 // Define work schedules
-const workSchedule1 = new WorkSchedule({
-    id: 1,
-    user: user1,
-    mondayHours: 8,
-    tuesdayHours: 8,
-    wednesdayHours: 8,
-    thursdayHours: 8,
-    fridayHours: 8,
-    saturdayHours: 0,
-    sundayHours: 0,
-});
+const workSchedules: WorkSchedule[] = [
+    new WorkSchedule({
+        id: 1,
+        user: users[0],
+        mondayHours: 8,
+        tuesdayHours: 8,
+        wednesdayHours: 8,
+        thursdayHours: 8,
+        fridayHours: 8,
+        saturdayHours: 0,
+        sundayHours: 0,
+    }),
+    new WorkSchedule({
+        id: 2,
+        user: users[1],
+        mondayHours: 7,
+        tuesdayHours: 7,
+        wednesdayHours: 7,
+        thursdayHours: 7,
+        fridayHours: 5,
+        saturdayHours: 0,
+        sundayHours: 0,
+    }),
+    new WorkSchedule({
+        id: 3,
+        user: users[2],
+        mondayHours: 6,
+        tuesdayHours: 6,
+        wednesdayHours: 8,
+        thursdayHours: 8,
+        fridayHours: 6,
+        saturdayHours: 4,
+        sundayHours: 0,
+    }),
+    new WorkSchedule({
+        id: 4,
+        user: users[3],
+        mondayHours: 8,
+        tuesdayHours: 8,
+        wednesdayHours: 6,
+        thursdayHours: 6,
+        fridayHours: 4,
+        saturdayHours: 0,
+        sundayHours: 0,
+    }),
+    new WorkSchedule({
+        id: 5,
+        user: users[4],
+        mondayHours: 7,
+        tuesdayHours: 7,
+        wednesdayHours: 7,
+        thursdayHours: 8,
+        fridayHours: 8,
+        saturdayHours: 0,
+        sundayHours: 0,
+    }),
+    new WorkSchedule({
+        id: 6,
+        user: users[5],
+        mondayHours: 6,
+        tuesdayHours: 5,
+        wednesdayHours: 8,
+        thursdayHours: 8,
+        fridayHours: 7,
+        saturdayHours: 3,
+        sundayHours: 0,
+    })
+];
 
-const workSchedule2 = new WorkSchedule({
-    id: 2,
-    user: user2,
-    mondayHours: 7,
-    tuesdayHours: 7,
-    wednesdayHours: 7,
-    thursdayHours: 7,
-    fridayHours: 5,
-    saturdayHours: 0,
-    sundayHours: 0,
-});
+// Set each user's work schedule
+users[0].setWorkSchedule(workSchedules[0]);
+users[1].setWorkSchedule(workSchedules[1]);
+users[2].setWorkSchedule(workSchedules[2]);
+users[3].setWorkSchedule(workSchedules[3]);
+users[4].setWorkSchedule(workSchedules[4]);
+users[5].setWorkSchedule(workSchedules[5]);
 
-const workSchedule3 = new WorkSchedule({
-    id: 3,
-    user: user3,
-    mondayHours: 6,
-    tuesdayHours: 6,
-    wednesdayHours: 8,
-    thursdayHours: 8,
-    fridayHours: 6,
-    saturdayHours: 4,
-    sundayHours: 0,
-});
+// Set user for each work schedule
+workSchedules[0].setUser(users[0]);
+workSchedules[1].setUser(users[1]);
+workSchedules[2].setUser(users[2]);
+workSchedules[3].setUser(users[3]);
+workSchedules[4].setUser(users[4]);
+workSchedules[5].setUser(users[5]);
 
-// Set work schedules to users
-user1.setWorkSchedule(workSchedule1);
-user2.setWorkSchedule(workSchedule2);
-user3.setWorkSchedule(workSchedule3);
+// Set up project and user relationships
+users[0].addProject(projects[0]);
+users[0].addProject(projects[1]);
+projects[0].addUser(users[0]);
+projects[1].addUser(users[0]);
 
-workSchedule1.setUser(user1);
-workSchedule2.setUser(user2);
-workSchedule3.setUser(user3);
+users[1].addProject(projects[0]);
+users[1].addProject(projects[1]);
+users[1].addProject(projects[2]);
+projects[0].addUser(users[1]);
+projects[1].addUser(users[1]);
+projects[2].addUser(users[1]);
 
-// Assign projects to users and vice versa
-user1.addProject(projectA);
-user1.addProject(projectB);
-projectA.addUser(user1);
-projectB.addUser(user1);
+users[2].addProject(projects[0]);
+users[2].addProject(projects[2]);
+projects[0].addUser(users[2]);
+projects[2].addUser(users[2]);
 
-user2.addProject(projectB);
-user2.addProject(projectC);
-projectB.addUser(user2);
-projectC.addUser(user2);
+users[3].addProject(projects[0]);
+users[3].addProject(projects[3]);
+users[3].addProject(projects[4]);
+projects[0].addUser(users[3]);
+projects[3].addUser(users[3]);
+projects[4].addUser(users[3]);
 
-user3.addProject(projectA);
-user3.addProject(projectC);
-projectA.addUser(user3);
-projectC.addUser(user3);
+users[4].addProject(projects[0]);
+users[4].addProject(projects[1]);
+users[4].addProject(projects[4]);
+projects[0].addUser(users[4]);
+projects[1].addUser(users[4]);
+projects[4].addUser(users[4]);
 
-// Create a work day with time blocks
-const workDays: WorkDay[] = [];  // Array to store created work days
-const timeBlocks: TimeBlock[] = [];  // Array to store created time blocks
+users[5].addProject(projects[0]);
+users[5].addProject(projects[2]);
+users[5].addProject(projects[3]);
+projects[0].addUser(users[5]);
+projects[2].addUser(users[5]);
+projects[3].addUser(users[5]);
+
+const workDays: WorkDay[] = [];
+const timeBlocks: TimeBlock[] = [];
 
 const createWorkDayWithTimeBlocks = (user: User, date: string, timeBlockData: { start: string, end: string, project: Project }[]) => {
     const workDay = new WorkDay({
@@ -164,52 +274,63 @@ const createWorkDayWithTimeBlocks = (user: User, date: string, timeBlockData: { 
             project: block.project,
             workday: workDay
         });
-        timeBlocks.push(timeBlock);  // Store created time blocks
+        timeBlocks.push(timeBlock);
         return timeBlock;
     });
 
     timeBlocksArray.forEach((timeBlock) => workDay.addTimeBlock(timeBlock));
-    workDays.push(workDay);  // Store created work days
+    workDays.push(workDay);
     user.addWorkDay(workDay);
 };
 
 // Create work days for users
-createWorkDayWithTimeBlocks(user1, '2024-10-30', [
-    { start: '2024-10-30T09:00:00', end: '2024-10-30T10:00:00', project: projectA },
-    { start: '2024-10-30T10:30:00', end: '2024-10-30T12:00:00', project: projectB },
-    { start: '2024-10-30T13:00:00', end: '2024-10-30T14:30:00', project: projectA },
+createWorkDayWithTimeBlocks(users[0], '2024-10-30', [
+    { start: '2024-10-30T09:00:00', end: '2024-10-30T10:00:00', project: projects[0] },
+    { start: '2024-10-30T10:30:00', end: '2024-10-30T12:00:00', project: projects[1] },
+    { start: '2024-10-30T13:00:00', end: '2024-10-30T14:30:00', project: projects[0] },
 ]);
 
-createWorkDayWithTimeBlocks(user2, '2024-10-30', [
-    { start: '2024-10-30T08:00:00', end: '2024-10-30T09:30:00', project: projectB },
-    { start: '2024-10-30T10:00:00', end: '2024-10-30T11:00:00', project: projectC },
-    { start: '2024-10-30T13:30:00', end: '2024-10-30T15:00:00', project: projectB },
+createWorkDayWithTimeBlocks(users[1], '2024-10-30', [
+    { start: '2024-10-30T08:00:00', end: '2024-10-30T09:30:00', project: projects[1] },
+    { start: '2024-10-30T10:00:00', end: '2024-10-30T11:00:00', project: projects[2] },
+    { start: '2024-10-30T13:30:00', end: '2024-10-30T15:00:00', project: projects[1] },
 ]);
 
-createWorkDayWithTimeBlocks(user3, '2024-10-30', [
-    { start: '2024-10-30T09:00:00', end: '2024-10-30T10:30:00', project: projectA },
-    { start: '2024-10-30T11:00:00', end: '2024-10-30T12:30:00', project: projectC },
-    { start: '2024-10-30T14:00:00', end: '2024-10-30T15:30:00', project: projectA },
+createWorkDayWithTimeBlocks(users[2], '2024-10-30', [
+    { start: '2024-10-30T09:00:00', end: '2024-10-30T10:30:00', project: projects[0] },
+    { start: '2024-10-30T11:00:00', end: '2024-10-30T12:30:00', project: projects[2] },
+    { start: '2024-10-30T14:00:00', end: '2024-10-30T15:30:00', project: projects[0] },
+]);
+
+createWorkDayWithTimeBlocks(users[3], '2024-10-30', [
+    { start: '2024-10-30T08:00:00', end: '2024-10-30T10:00:00', project: projects[3] },
+    { start: '2024-10-30T10:30:00', end: '2024-10-30T12:30:00', project: projects[4] },
+    { start: '2024-10-30T13:00:00', end: '2024-10-30T15:00:00', project: projects[3] },
+]);
+
+createWorkDayWithTimeBlocks(users[4], '2024-10-30', [
+    { start: '2024-10-30T09:00:00', end: '2024-10-30T11:00:00', project: projects[1] },
+    { start: '2024-10-30T11:30:00', end: '2024-10-30T13:00:00', project: projects[4] },
+    { start: '2024-10-30T13:30:00', end: '2024-10-30T15:00:00', project: projects[1] },
+]);
+
+createWorkDayWithTimeBlocks(users[5], '2024-10-30', [
+    { start: '2024-10-30T08:00:00', end: '2024-10-30T09:30:00', project: projects[2] },
+    { start: '2024-10-30T10:00:00', end: '2024-10-30T11:30:00', project: projects[3] },
+    { start: '2024-10-30T13:00:00', end: '2024-10-30T14:30:00', project: projects[2] },
 ]);
 
 // Create current work day
 const createCurrentWorkday = (user: User) => {
     const currentDate = new Date().toISOString().split('T')[0];
     const timeBlockData = [
-        { start: `${currentDate}T09:00:00`, end: `${currentDate}T12:00:00`, project: projectA },
-        { start: `${currentDate}T13:00:00`, end: `${currentDate}T17:00:00`, project: projectB }
+        { start: `${currentDate}T09:00:00`, end: `${currentDate}T12:00:00`, project: projects[0] }
     ];
     createWorkDayWithTimeBlocks(user, currentDate, timeBlockData);
 };
 
-// Create current work days for each user
-createCurrentWorkday(user1);
-createCurrentWorkday(user2);
-createCurrentWorkday(user3);
-
-const users = [user1, user2, user3];
-const projects = [projectA, projectB, projectC];
-const workSchedules = [workSchedule1, workSchedule2, workSchedule3];
+// Create current workday for each user
+users.forEach(createCurrentWorkday);
 
 export {
     users,
