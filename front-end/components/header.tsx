6ef95144d38ff5a2318311e143dbab2c3f44479e
Roel Crabbé <crabberoel@gmail.com>
2024-11-03 18:41:31 +0100

    update a bit of css

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
+  const router = useRouter();
+
   return (
-    <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
-      <a className="fs-2 d-flex justify-content-center mb-2 mb-lg-0 text-white-50 text-decoration-none">
-        Time Tracker
-      </a>
+    <header className="d-flex justify-content-between p-3 border-bottom bg-dark bg-gradient">
       <nav className="nav justify-content-center">
-        <Link href="/" className="nav-link px-4 fs-5 text-white">
+        <Link href="/" className={`nav-link px-4 fs-5 text-white ${router.pathname === '/' ? 'border-bottom' : ''}`}>
           Home
         </Link>
-        <Link href="/projects" className="nav-link px-4 fs-5 text-white">
+        <Link href="/projects" className={`nav-link px-4 fs-5 text-white ${router.pathname === '/projects' ? 'border-bottom' : ''}`}>
           Projects
         </Link>
       </nav>
+      <a className="fs-2 d-flex justify-content-center text-white-50 text-decoration-none">
+        Time Tracker
+      </a>
     </header>
   );
 };
