d1b820f0cce34db4f6903d771754f28ac0346dec
Roel Crabbé <crabberoel@gmail.com>
2024-11-23 14:19:38 +0100

    some security

--- back-end/app.ts
+++ back-end/app.ts
@@ -9,22 +9,33 @@ import { projectRouter } from './controller/project.routes';
 import { userRouter } from './controller/user.routes';
 import { workDayRouter } from './controller/workDay.routes';
 import { timeBlockRouter } from './controller/timeBlock.routes';
+import { expressjwt } from 'express-jwt';
 
 const app = express();
+
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
 app.use(cors({ origin: 'http://localhost:8080' }));
 app.use(bodyParser.json());
 
-app.get('/status', (req, res) => {
-    res.json({ message: 'Time Tracker API Running...' });
-});
+app.use(
+    expressjwt({
+        secret: process.env.JWT_SECRET || 'default_secret',
+        algorithms: ['HS256'],
+    }).unless({
+        path: ['/api-docs', /^\/api-docs\/.*/, '/users/signup', '/users/login', '/status'],
+    })
+);
 
 app.use('/projects', projectRouter);
 app.use('/users', userRouter);
 app.use('/workdays', workDayRouter);
 app.use('/timeblocks', timeBlockRouter);
+
+app.get('/status', (req, res) => {
+    res.json({ message: 'Time Tracker API Running...' });
+});
 
 const swaggerOpts = {
     definition: {
@@ -52,4 +63,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.listen(port || 3000, () => {
     console.log(`Time Tracker API Running on port ${port}.`);
     console.log(`Swagger running on http://localhost:${port}/api-docs.`);
+    console.log(`Check Tokens on https://jwt.io.`);
 });
